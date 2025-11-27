690b0a916fb7c8be559148ee2f4da9b0adadd45f
Mahbub Hasan <mahbub.hb.dev@gmail.com>
2025-11-27 11:47:09 +0600

    Styles have been added

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -5,8 +5,8 @@ const FAQ = ({title, desc}) => {
     const [toggle, setToggle] = useState(false);
 
   return (
-    <section>
-        <div>
+    <section className='bg-gray-200 mb-3 p-3 shadow shadow-gray-500 rounded-lg'>
+        <div className='flex justify-between items-center'>
             <h3 className='text-2xl font-medium'> {title} </h3>
             <button onClick={() => {setToggle(!toggle)}} className='border text-xl rounded-md font-bold w-10'> {toggle ? "-" : "+"} </button>
         </div>
